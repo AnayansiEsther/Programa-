113d880ab72313303130e8657b7f93be979dd31a
AnayansiQ <quirosanayansi06@gmail.com>
2025-09-21 17:49:46 -0500

    Soy A

--- v_5.1.1.py
+++ v_5.1.1.py
@@ -3,3 +3,8 @@
 print("Hola mundo")
 print("En realidad estoy trasteando esta cosa")
 print("Estoy intentando ser la persona B:(")
+<<<<<<< HEAD
+=======
+print("Estamos teniendo problemas tecnicos")
+
+>>>>>>> a6059b8 (Soy A)
